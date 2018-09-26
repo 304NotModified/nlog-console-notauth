843882ea3a060cfe453a7a2440ea08d02b8d6ff0
Julian Verdurmen <304NotModified@users.noreply.github.com>
2018-09-26 17:48:42 +0200

    init

---
+++ ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public void Foo()
+        {
+            Logger.Info("log from Class1.Foo()");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using NLog;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var logger = LogManager.GetLogger("logger1");
+
+            logger.Info("message from main()");
+
+            new Class1().Foo();
+
+            Console.WriteLine("press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
